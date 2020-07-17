69763568031c5244bb016bea8765a6be2f0e9d9a
LockheadDev <68433067+LockheadDev@users.noreply.github.com>
2020-07-17 17:49:18 -0500

    Help Menu implemented at first run
    -Now reset button will quit the game and reset ALL saved data in the game

---
+++ Assets/Code/FirstTimeLatch.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstTimeLatch : MonoBehaviour
+{
+    public GameObject[] hidelist;
+     public GameObject[] showlist;
+    int firstRun =0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        firstRun = PlayerPrefs.GetInt("firstRun");
+        if(firstRun==0)
+        {
+            firstRun=1;
+            PlayerPrefs.SetInt("firstRun",1);
+            for(int i=0; i<hidelist.Length;i++)
+            {
+                hidelist[i].SetActive(false);
+            }
+
+            for(int i=0 ; i<showlist.Length;i++)
+            {
+                showlist[i].SetActive(true);
+            }
+        }
+        else
+        {
+        for(int i=0; i<hidelist.Length;i++)
+            {
+                hidelist[i].SetActive(true);
+            }
+
+            for(int i=0 ; i<showlist.Length;i++)
+            {
+                showlist[i].SetActive(false);
+            }
+        }
+    }
+
+    // Update is called once per frame
+}
