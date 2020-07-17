3645b9500f379f3f1e2cc6aa3eacff97be6f9507
LockheadDev <68433067+LockheadDev@users.noreply.github.com>
2020-07-17 14:47:48 -0500

    Added Feedbak link
    Also minor improvement to stats table

---
+++ Assets/Code/FeedbackButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FeedbackButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void OpenURL()
+    {
+        Application.OpenURL("https://forms.gle/SvezdxnR6h7RUXTa6");
+        Debug.Log("Opening feedback link...");
+    }
+}
